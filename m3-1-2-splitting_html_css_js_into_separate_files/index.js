9685941c405d05e215422543e2cd6871ca788715
bavana.edu@gmail.com <bavana.edu@gmail.com>
2024-10-13 12:30:38 +0530

    commited returning hier ... elements

--- m3-1-2-splitting_html_css_js_into_separate_files/index.js
+++ m3-1-2-splitting_html_css_js_into_separate_files/index.js
@@ -1,1 +1,7 @@
 // Bring your JavaScript here
+const logoDiv = document.querySelector('#logo')
+    const logoImg = document.createElement('img')
+    logoDiv.appendChild(logoImg)
+    logoDiv.style.maxWidth = '100px'
+    logoImg.src = 'logo.png'
+    logoImg.style.width = '100%'
